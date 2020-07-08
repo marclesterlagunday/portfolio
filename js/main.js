2901c8324cbef579444651cd4efb74fbfa4b6d80
Lester <marclesterlagunday@gmail.com>
2020-07-08 15:09:54 +0800

    parallax testing

--- js/main.js
+++ js/main.js
@@ -14,4 +14,12 @@ $(document).ready(function(){
 		.removeClass('visibility-hidden')
 		.addClass('animate__animated animate__flipInY');
 	}, 1800);
+	responsive_css();
 });
+
+function responsive_css(){
+	let primary_header_margin_top = $(".primary-header").css('margin-top').replace(/[^0-9]/g,'');
+	let navbar_height = $("nav").height();
+
+	$(".primary-header").css('margin-top',  (parseInt(primary_header_margin_top) + parseInt(navbar_height)) + "px");
+}
